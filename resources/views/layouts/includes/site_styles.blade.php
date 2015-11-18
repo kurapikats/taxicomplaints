3c12238f114045010aeb43425d49c03549df673c
kurapikats <kurapikats@yahoo.com>
2015-11-18 22:23:52 +0800

    now using minified css

--- resources/views/layouts/includes/site_styles.blade.php
+++ resources/views/layouts/includes/site_styles.blade.php
@@ -1,7 +1,7 @@
 {{-- Included Styles --}}
-{!! Html::style('components/bootstrap/dist/css/bootstrap.css') !!}
-{!! Html::style('components/font-awesome/css/font-awesome.css') !!}
-{!! Html::style('components/bootstrap-datepicker/dist/css/bootstrap-datepicker.css') !!}
+{!! Html::style('components/bootstrap/dist/css/bootstrap.min.css') !!}
+{!! Html::style('components/font-awesome/css/font-awesome.min.css') !!}
+{!! Html::style('components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') !!}
 {!! Html::style('http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic') !!}
 
 {!! Html::style('components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css') !!}
