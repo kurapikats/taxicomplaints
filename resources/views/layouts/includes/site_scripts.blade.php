92ddbf207d8be6053fa7a9d0b0609dc5d8af7d65
kurapikats <kurapikats@yahoo.com>
2015-12-02 15:59:57 +0800

    code cleanup

--- resources/views/layouts/includes/site_scripts.blade.php
+++ resources/views/layouts/includes/site_scripts.blade.php
@@ -8,15 +8,5 @@
 @if (env('APP_ENV') === 'local')
     {!! Html::script('components/less/dist/less.min.js') !!}
 @else
-    {{-- begin google analytics --}}
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-70277228-1', 'auto');
-      ga('send', 'pageview');
-    </script>
-    {{-- end google analytics --}}
+    @include('layouts.includes.google_analytics')
 @endif
